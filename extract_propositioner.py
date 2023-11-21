97dc6c27bc8bb62ae3ca74cb03c65fd4ef243734
Dennis Priskorn <egils@riseup.net>
2023-11-21 08:25:40 +0100

    chore: remove unused dependency and format code

--- extract_propositioner.py
+++ extract_propositioner.py
@@ -1,8 +1,6 @@
 from models.riksdagen_analyzer import RiksdagenAnalyzer
 
 
-ra = RiksdagenAnalyzer(
-    workdirectory="data/sv/propositioner", filename="propositioner"
-)
+ra = RiksdagenAnalyzer(workdirectory="data/sv/propositioner", filename="propositioner")
 ra.start()
 # print(ra.df)
