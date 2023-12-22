c3d4ff60ee56c3baa25c5cdae1fa2fceab36bfcf
Dennis Priskorn <egils@riseup.net>
2023-12-22 10:04:52 +0100

    chore: Default to warn level

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 fasttext_model = "lid.176.bin"
 fasttext_model_download_date = datetime.strptime("2023-12-21", "%Y-%m-%d").date()
-loglevel = logging.DEBUG
+loglevel = logging.WARN
