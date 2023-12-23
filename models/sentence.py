444f4f4548709ce2a44a3f19d46f8dbbbfd871ae
Dennis Priskorn <egils@riseup.net>
2023-12-23 04:08:35 +0100

    fix: Improve cleaning of sentences to help the model detect the language. Also skip the detection for empty strings.

--- models/sentence.py
+++ models/sentence.py
@@ -79,11 +79,14 @@ class Sentence(BaseModel):
     @property
     def cleaned_sentence(self) -> str:
         # Remove newlines, digits and a selection of characters
-        return re.sub(
+        sentence = self.text
+        sentence = re.sub(
             r"\d+",
             "",
-            self.sent.text.replace("\n", "")
-            .replace("\r", "")
+            sentence,
+        )
+        sentence = (
+            sentence.replace("\t", " ")
             .replace(":", "")
             .replace(",", "")
             .replace(".", "")
@@ -92,28 +95,48 @@ class Sentence(BaseModel):
             .replace("-", "")
             .replace("–", "")
             .replace("/", "")
-            .strip(),
         )
+        sentence = " ".join(sentence.split())
+        return sentence.strip()
+
+    @property
+    def has_content_after_cleaning(self) -> bool:
+        return bool(self.cleaned_sentence)
 
     def analyze_and_insert(self):
-        self.clean_and_print_sentence()
         # We insert and store valuable tokens even if the
         # sentence is not deemed suitable for our purposes
-        self.detect_language()
-        self.insert_score()
-        self.iterate_tokens()
-        if self.is_suitable_sentence:
-            self.generate_uuid()
-            read = Read()
-            read.connect_and_setup()
-            sentence_id = read.get_sentence_id(sentence=self)
-            read.close_db()
-            if not sentence_id:
-                insert = Insert()
-                insert.connect_and_setup()
-                insert.insert_sentence(sentence=self)
-                insert.link_sentence_to_rawtokens(sentence=self)
-                insert.close_db()
+        # Language detection is needed to find out
+        # it if already exists in the database
+        if not self.has_content_after_cleaning:
+            logger.warning("Skipped empty sentence")
+        else:
+            self.detect_language()
+            self.insert_score()
+            self.iterate_tokens()
+            # We don't trust scores below this threshold
+            # They are often occur when only one or two tokens
+            # are left after cleaning
+            if self.score >= 0.4:
+                sentence_id = self.id
+                if not sentence_id:
+                    if self.is_suitable_sentence:
+                        self.generate_uuid()
+                        insert = Insert()
+                        insert.connect_and_setup()
+                        insert.insert_sentence(sentence=self)
+                        insert.link_sentence_to_rawtokens(sentence=self)
+                        insert.close_db()
+                    else:
+                        logger.info("Skipping unsuitable sentence")
+                else:
+                    logger.info("Skipping sentence we already have analyzed")
+            else:
+                logger.warning(
+                    "Skipping sentence with language detection score below 0.4:\n"
+                    f"lang: {self.detected_language}\n"
+                    f"sentence: {self.cleaned_sentence}"
+                )
 
     def insert_score(self):
         read = Read()
@@ -133,11 +156,11 @@ class Sentence(BaseModel):
             if token.is_accepted_token:
                 self.accepted_tokens.append(token)
 
-    def clean_and_print_sentence(self):
-        logger.info(
-            f"Sentence text: {self.cleaned_sentence}, "
-            f"Token count: {self.cleaned_sentence.split()}"
-        )
+    # def clean_and_print_sentence(self):
+    #     logger.info(
+    #         f"Sentence text: {self.cleaned_sentence}, "
+    #         f"Token count: {self.cleaned_sentence.split()}"
+    #     )
 
     def generate_uuid(self) -> None:
         # Generate UUIDs for each sentence and add them to a new 'uuid' column
@@ -146,7 +169,16 @@ class Sentence(BaseModel):
     def detect_language(self) -> None:
         """Detect language using fasttext"""
         # This returns a dict like so: {'lang': 'tr', 'score': 0.9982126951217651}
-        result = detect(text=self.text.replace("\n", ""), low_memory=False)
-        # We round the score because the extra decimals do not add anything of value
-        self.score = round(result["score"], 2)
-        self.detected_language = result["lang"]
+        # We clean the sentence before language detection to avoid garbage results
+        cleaned_sentence = self.cleaned_sentence
+        if cleaned_sentence:
+            result = detect(text=cleaned_sentence, low_memory=False)
+            # if result["score"] < 0.4:
+            #     # This can be caused by the cleaning,
+            #     # so try alternative cleaning where newlines
+            #     # are replaced with spaces
+            # We round the score because the extra decimals do not add anything of value
+            self.score = round(result["score"], 2)
+            self.detected_language = result["lang"]
+        else:
+            logger.warning(f"No content after cleaning, skipping language detection")
