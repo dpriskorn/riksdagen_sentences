11493453830853f57af39c24a9ea008b359e718b
Dennis Priskorn <egils@riseup.net>
2023-12-24 16:49:25 +0100

    chore: More cleaning todo

--- models/crud/delete.py
+++ models/crud/delete.py
@@ -3,4 +3,9 @@ from models.crud.database_handler import Mariadb
 
 class Delete(Mariadb):
     # TODO delete rawtokens and normtokens with unaccepted chars
+    # TODO delete garbage sentences with unaccepted token chars
+    """SELECT *
+    FROM sentence s
+    WHERE text LIKE '%¥%';
+    """
     pass
